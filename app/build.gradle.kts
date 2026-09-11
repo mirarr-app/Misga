46f7d16a8cb5f906d30c175bddbce530ad59530e
parsamrrelax <parsa.osunlou1998@gmail.com>
2026-09-11 14:11:39 +0330

    chore: bump app version to 1.3.0 (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.miss.ga"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.2.1"
+        versionCode = 14
+        versionName = "1.3.0"
     }
 
     signingConfigs {
