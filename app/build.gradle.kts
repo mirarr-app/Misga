6e7efaf688703aacebe2b71b88a62537b7223cc8
parsamrrelax <parsa.osunlou1998@gmail.com>
2026-09-16 11:31:02 +0330

    chore: bump version to 1.3.6 (versionCode 20) and add release changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.miss.ga"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.3.5"
+        versionCode = 20
+        versionName = "1.3.6"
     }
 
     signingConfigs {
